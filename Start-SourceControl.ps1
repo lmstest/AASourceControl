8c59b10aa973a3c44f64dff9d347de2da7760ad7
lmstest <36256940+lmstest@users.noreply.github.com>
2021-12-14 19:45:16 -0800

    Update Start-SourceControl.ps1

--- Start-SourceControl.ps1
+++ Start-SourceControl.ps1
@@ -3,5 +3,5 @@ workflow Start-SourceControl
         #V1.0    
         #This is my first Source Control Runbook    
         
-        Write-Output "Azue Automation"
+        Write-Output "Azue Automation with Powershell Workflow"
 }
